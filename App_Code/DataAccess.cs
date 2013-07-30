00b03b3b6a15c305f7fd4124bc6d1222b38ca8a7
Marsen Lin <thisismysoul@gmail.com>
2013-07-30 17:47:41 +0800

    add DataAccess method
    ExecuteSQLForSQL

--- App_Code/DataAccess.cs
+++ App_Code/DataAccess.cs
@@ -171,6 +171,43 @@ public class DataAccess
     /// </summary>
     /// <param name="cnString">連線字串</param>
     /// <param name="sqlString">SQL語法</param>
+    /// <param name="parameter">查詢參數</param>
+    /// <returns>執行是否成功?</returns>
+    public static bool ExecuteSQLForSQL(string cnString, string sqlString, SqlParameter parameter) {
+        SqlConnection conn = null;
+        bool result = false;
+
+        try
+        {
+            conn = new SqlConnection(cnString);
+            conn.Open();
+
+            var cm = new SqlCommand(sqlString, conn);
+            cm.Parameters.Clear();
+            cm.Parameters.Add(parameter);            
+            cm.CommandTimeout = 180;
+            cm.ExecuteNonQuery();
+            result = true;
+        }
+        catch (Exception ex)
+        {
+            //TODO: catch exception
+        }
+        finally
+        {
+            if (conn != null && conn.State != ConnectionState.Closed)
+            {
+                conn.Close();
+            }
+        }
+
+        return result;
+    }
+    /// <summary>
+    /// ExecuteSQLForSQL
+    /// </summary>
+    /// <param name="cnString">連線字串</param>
+    /// <param name="sqlString">SQL語法</param>
     /// <param name="_params">參數陣列</param>
     /// <returns>執行是否成功?</returns>
     public static bool ExecuteSQLForSQL(string cnString, string sqlString, SqlParameter[] _params)
